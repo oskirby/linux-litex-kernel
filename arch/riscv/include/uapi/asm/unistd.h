56f8f9b16100d9615700aef2afef362f3f58cc2a
Antony Pavlov <antonynpavlov@gmail.com>
2020-04-04 00:31:12 +0300

    Revert "riscv: Use latest system call ABI"
    This reverts commit d4c08b9776b392e20efc6198ebe1bc8ec1911d9b.

--- arch/riscv/include/uapi/asm/unistd.h
+++ arch/riscv/include/uapi/asm/unistd.h
@@ -20,6 +20,10 @@
 #define __ARCH_WANT_SET_GET_RLIMIT
 #define __ARCH_WANT_SYS_CLONE3
 #endif /* __LP64__ */
+#define __ARCH_WANT_SET_GET_RLIMIT
+#ifndef __LP64__
+#define __ARCH_WANT_TIME32_SYSCALLS
+#endif
 
 #include <asm-generic/unistd.h>
 
