9083c775df0b0f148a9f0b272431d2886540984b
Owen Kirby <oskirby@gmail.com>
2020-07-11 02:54:53 -0700

    spi: Fix memory corruption bug in spi-litespi.c
    Incrementing the tx_buf and rx_buf pointers in the SPI transfer
    function can lead to memory corruption in the user of the SPI
    device if it was expecting the pointeres to remain constant. This
    was responsible for causing a kernel panic in the mmc-spi driver.
    
    Gabriel Solmo, in his litex-rocket-rebase branch has already fixed
    this bug, so I have really just merged his driver into the vexriscv
    branch for the time being until I can get his kernel booting again
    on a vexriscv platform.
    
    Signed-off-by: Owen Kirby <oskirby@gmail.com>

--- drivers/spi/spi-litespi.c
+++ drivers/spi/spi-litespi.c
@@ -31,99 +31,93 @@
 
 struct litespi_hw {
 	struct spi_master *master;
-	void __iomem *base_addr;
-	struct mutex bus_mutex;
+	void __iomem *base;
 };
 
 static inline void litespi_wait_xfer_end(struct litespi_hw *hw)
 {
-	while (!litex_get_reg(hw->base_addr + LITESPI_OFF_STAT,
+	while (!litex_get_reg(hw->base + LITESPI_OFF_STAT,
 			      LITESPI_SZ_STAT))
 		cpu_relax();
 }
 
-static void litespi_rxtx(struct litespi_hw *hw, struct spi_transfer *t)
-{
-	u32 val;
-	u8 bytes;
-	int i;
-
-	/*
-	 * Calculate number of bytes necessary for given bits_per_word value.
-	 * Incrementing by 7 forces non-multiplies of 8 to be incremented by 1
-	 * after bit shifting, without changing the result for multiplies of 8.
-	 */
-	bytes = (t->bits_per_word + 7) >> 3;
-
-	for (i = 0; i < t->len; i += bytes) {
-		if (t->tx_buf) {
-			memcpy(&val, t->tx_buf, bytes);
-			litex_set_reg(hw->base_addr + LITESPI_OFF_MOSI, bytes,
-				      val);
-			t->tx_buf += bytes;
-		}
-
-		val = litex_get_reg(hw->base_addr + LITESPI_OFF_CTRL,
-				    LITESPI_SZ_CTRL);
-		litex_set_reg(hw->base_addr + LITESPI_OFF_CTRL, LITESPI_SZ_CTRL,
-			      val | BIT(LITESPI_CTRL_START_BIT));
-		litespi_wait_xfer_end(hw);
-
-		if (t->rx_buf) {
-			val = litex_get_reg(hw->base_addr + LITESPI_OFF_MISO,
-					    bytes);
-			memcpy(t->rx_buf, &val, bytes);
-			t->rx_buf += bytes;
-		}
-	}
-}
-
-static int litespi_xfer_one(struct spi_master *master, struct spi_message *m)
+static int litespi_rxtx(struct spi_master *master, struct spi_device *spi,
+			struct spi_transfer *t)
 {
 	struct litespi_hw *hw = spi_master_get_devdata(master);
-	struct spi_transfer *t;
+	u16 ctl_word = t->bits_per_word << LITESPI_CTRL_SHIFT_BPW;
+	int i;
 
-	mutex_lock(&hw->bus_mutex);
+	/* set chip select */
+	litex_set_reg(hw->base + LITESPI_OFF_CS, LITESPI_SZ_CS, BIT(spi->chip_select));
 
-	/* setup chip select */
-	litex_set_reg(hw->base_addr + LITESPI_OFF_CS, LITESPI_SZ_CS,
-		      BIT(m->spi->chip_select));
+	/* set word size */
+	litex_set_reg(hw->base + LITESPI_OFF_CTRL, LITESPI_SZ_CTRL, ctl_word);
 
-	list_for_each_entry(t, &m->transfers, transfer_list) {
-		litespi_rxtx(hw, t);
-		m->actual_length += t->len;
+	/* add start bit to ctl_word */
+	ctl_word |= BIT(LITESPI_CTRL_START_BIT);
+
+	/*
+	 * Validated SPI transfer length is multiple of SPI word size, which
+	 * is itself a power-of-two multiple, and fits within LITEX_SUBREG_SIZE
+	 */
+	if (t->bits_per_word <= 8) {
+		const u8 *tx = t->tx_buf;
+		u8 *rx = t->rx_buf;
+
+		/* word size is 1 byte */
+		for (i = 0; i < t->len; i++) {
+			if (tx)
+				litex_set_reg(hw->base + LITESPI_OFF_MOSI, 1, *tx++);
+
+			litex_set_reg(hw->base + LITESPI_OFF_CTRL, LITESPI_SZ_CTRL, ctl_word);
+			litespi_wait_xfer_end(hw);
+
+			if (rx)
+				*rx++ = litex_get_reg(hw->base + LITESPI_OFF_MISO, 1);
+		}
+	} else if (t->bits_per_word <= 16) {
+		const u16 *tx = t->tx_buf;
+		u16 *rx = t->rx_buf;
+
+		/* word size is 2 bytes */
+		for (i = 0; i < t->len / 2; i++) {
+			if (tx)
+				litex_set_reg(hw->base + LITESPI_OFF_MOSI, 2,
+						 be16_to_cpu(*tx++));
+
+			litex_set_reg(hw->base + LITESPI_OFF_CTRL, LITESPI_SZ_CTRL, ctl_word);
+			litespi_wait_xfer_end(hw);
+
+			if (rx)
+				*rx++ = cpu_to_be16(litex_get_reg(hw->base + LITESPI_OFF_MISO, 2));
+		}
+	} else {
+		const u32 *tx = t->tx_buf;
+		u32 *rx = t->rx_buf;
+
+		/* word size is 4 bytes */
+		for (i = 0; i < t->len / 4; i++) {
+			if (tx)
+				litex_set_reg(hw->base + LITESPI_OFF_MOSI, 4,
+						 be32_to_cpu(*tx++));
+
+			litex_set_reg(hw->base + LITESPI_OFF_CTRL, LITESPI_SZ_CTRL, ctl_word);
+			litespi_wait_xfer_end(hw);
+
+			if (rx)
+				*rx++ = cpu_to_be32(litex_get_reg(hw->base + LITESPI_OFF_MISO, 4));
+		}
 	}
-
-	m->status = 0;
-	spi_finalize_current_message(master);
-
-	mutex_unlock(&hw->bus_mutex);
-
-	return 0;
-}
-
-static int litespi_setup(struct spi_device *spi)
-{
-	struct litespi_hw *hw = spi_master_get_devdata(spi->master);
-
-	/* lock and wait for transfer finish */
-	mutex_lock(&hw->bus_mutex);
-	litespi_wait_xfer_end(hw);
-
-	/* set word size and clear CS bits */
-	litex_set_reg(hw->base_addr + LITESPI_OFF_CTRL, LITESPI_SZ_CTRL,
-		      spi->bits_per_word << LITESPI_CTRL_SHIFT_BPW);
-
-	mutex_unlock(&hw->bus_mutex);
 
 	return 0;
 }
 
 static int litespi_probe(struct platform_device *pdev)
 {
+	struct device_node *node = pdev->dev.of_node;
 	struct litespi_hw *hw;
 	struct spi_master *master;
-	struct device_node *np;
 	struct resource *res;
 	int ret;
 	u32 val;
@@ -131,62 +125,59 @@ static int litespi_probe(struct platform_device *pdev)
 	if (!litex_check_accessors())
 		return -EPROBE_DEFER;
 
-	/* check if device tree exists */
-	np = pdev->dev.of_node;
-	if (!np)
-		return -ENODEV;
-
-	/* allocate memory for master */
 	master = spi_alloc_master(&pdev->dev, sizeof(*hw));
 	if (!master)
 		return -ENOMEM;
 
-	hw = spi_master_get_devdata(master);
-	hw->master = master;
-	mutex_init(&hw->bus_mutex);
-
-	hw->master->dev.of_node = pdev->dev.of_node;
-	hw->master->setup = litespi_setup;
-	hw->master->transfer_one_message = litespi_xfer_one;
-	hw->master->mode_bits =	SPI_MODE_0;
-	hw->master->flags = SPI_CONTROLLER_MUST_RX | SPI_CONTROLLER_MUST_TX;
+	master->dev.of_node = pdev->dev.of_node;
+	master->bus_num = pdev->id;
+	master->transfer_one = litespi_rxtx;
+	master->mode_bits = SPI_MODE_0 | SPI_CS_HIGH;
+	master->flags = SPI_CONTROLLER_MUST_RX | SPI_CONTROLLER_MUST_TX;
 
 	/* get bits per word property */
-	ret = of_property_read_u32(np, "litespi,max-bpw", &val);
-	if (ret || val > 32)
-		return -EINVAL;
-
-	hw->master->bits_per_word_mask = SPI_BPW_RANGE_MASK(1, val);
+	ret = of_property_read_u32(node, "litespi,max-bpw", &val);
+	if (ret)
+		goto err;
+	if (val > 32) {
+		ret = -EINVAL;
+		goto err;
+	}
+	master->bits_per_word_mask = SPI_BPW_RANGE_MASK(1, val);
 
 	/* get sck frequency */
-	ret = of_property_read_u32(np, "litespi,sck-frequency", &val);
+	ret = of_property_read_u32(node, "litespi,sck-frequency", &val);
 	if (ret)
-		return -EINVAL;
-
-	hw->master->min_speed_hz = val;
-	hw->master->max_speed_hz = val;
+		goto err;
+	master->max_speed_hz = val;
 
 	/* get num cs */
-	ret = of_property_read_u32(np, "litespi,num-cs", &val);
+	ret = of_property_read_u32(node, "litespi,num-cs", &val);
 	if (ret)
-		return -EINVAL;
+		goto err;
+	master->num_chipselect = val;
 
-	hw->master->num_chipselect = val;
+	hw = spi_master_get_devdata(master);
+	hw->master = master;
 
 	/* get base address */
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	hw->base_addr = devm_ioremap_resource(&pdev->dev, res);
-	if (!hw->base_addr)
-		return -ENXIO;
+	hw->base = devm_ioremap_resource(&pdev->dev, res);
+	if (IS_ERR(hw->base)) {
+		ret = PTR_ERR(hw->base);
+		goto err;
+	}
 
 	/* register controller */
 	ret = devm_spi_register_master(&pdev->dev, master);
-	if (ret < 0) {
-		spi_master_put(master);
-		return ret;
-	}
+	if (ret)
+		goto err;
 
 	return 0;
+
+err:
+	spi_master_put(master);
+	return ret;
 }
 
 static const struct of_device_id litespi_match[] = {
